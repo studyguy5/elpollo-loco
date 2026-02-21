e5192227de2134fe2c63837719c2f70f82820530
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-02-21 02:23:20 +0100

    insert objects into canvas

---
+++ models/floor.class.js
@@ -0,0 +1,12 @@
+class floor extends MovalbleObject{
+width = 720;
+height = 140;
+x;
+y;
+
+    constructor(x = 0, y = 0){
+        super().loadImage('img/5_background/layers/1_first_layer/1.png')
+        this.x = 0;
+        this.y = 340;      
+    }
+}
